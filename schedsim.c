7669d29087dd31d3f5eb0dc29e81ed30ca329c1f
Brent Matthew Ortizo <144473184+BrentMatthewOrtizo@users.noreply.github.com>
2026-04-15 16:50:49 -0700

    Made process.h and testing

--- schedsim.c
+++ schedsim.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h> // strcmp() changed since argv[1] is a string, compare strings with strcmp()
 
+#include "process.h"
+
 int main(int argc, char *argv[]) {
     int quantum;
 
@@ -55,6 +57,28 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[1], "rr") == 0) {
         printf("Time quantum: %d\n", quantum);
     }
+
+    // temp test struct 
+    Process test_process;
+
+    strcpy(test_process.pid, "P1");
+    test_process.arrival = 0;
+    test_process.burst = 8;
+    test_process.priority = 3;
+    test_process.remaining_burst = 8;
+    test_process.first_start_time = -1;
+    test_process.completion_time = -1;
+    test_process.started = 0;
+    test_process.finished = 0;
+    test_process.vruntime = 0;
+    test_process.rr_time_used = 0;
+    test_process.index = 0;
+
+    printf("\nTesting Process struct:\n");
+    printf("PID: %s\n", test_process.pid);
+    printf("Arrival: %d\n", test_process.arrival);
+    printf("Burst: %d\n", test_process.burst);
+    printf("Priority: %d\n", test_process.priority);
 
     return 0;
 }
