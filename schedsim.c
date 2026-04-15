c4cc6f1d8804045e61761b73766a174247948f0d
Brent Matthew Ortizo <144473184+BrentMatthewOrtizo@users.noreply.github.com>
2026-04-15 16:41:19 -0700

    Refactored code prototype

--- schedsim.c
+++ schedsim.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    int quantum;
 
-  // Command line validation:
-  if(argc > 4){
-    printf(stderr, "Incorrect argument count\n");
-    printf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file> <time quantum>");
-   return -1;
-  }
-
-  else{
-    if(argv[1] != ("priority" || "rr" || "cfs")){
-    printf(stderr, "Invalid scheduling algorithm\n");
-    printf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file> <time quantum>");
+    // Need at least program name + algorithm + input file
+    if (argc < 3 || argc > 4) {
+        fprintf(stderr, "Incorrect argument count.\n");
+        fprintf(stderr, "Usage:\n");
+        fprintf(stderr, "  ./schedsim priority <input_file>\n");
+        fprintf(stderr, "  ./schedsim cfs <input_file>\n");
+        fprintf(stderr, "  ./schedsim rr <input_file> <time_quantum>\n");
+        return 1;
     }
 
-    if(argv[1] == ("priority" || "cfs") && argc != 3){
-      printf(stderr, "Incorrect argument count\n");
-      printf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file>");
-      return -1;
+    // We validate algorithm
+    if (strcmp(argv[1], "priority") != 0 &&
+        strcmp(argv[1], "rr") != 0 &&
+        strcmp(argv[1], "cfs") != 0) {
+        fprintf(stderr, "Invalid scheduling algorithm.\n");
+        fprintf(stderr, "Valid options are: priority, rr, cfs\n");
+        return 1;
     }
-    if(argv[1] == "rr" && argc != 4){
-      printf(stderr, "Incorrect argument count\n");
-      printf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file> <time quantum>");
-      return -1;
 
-      if(atoi(argv[3]) <= 0){
-        printf(stderr, "Invalid time quantum\n");
-        printf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file> <time quantum>");
-        return -1;
-      }
+    // priority and cfs should have exactly 3 args
+    if ((strcmp(argv[1], "priority") == 0 || strcmp(argv[1], "cfs") == 0) && argc != 3) {
+        fprintf(stderr, "Incorrect argument count for %s.\n", argv[1]);
+        fprintf(stderr, "Usage: ./schedsim %s <input_file>\n", argv[1]);
+        return 1;
     }
-  }
-  return 0;
+
+    // rr should have exactly 4 args 
+    if (strcmp(argv[1], "rr") == 0) {
+        if (argc != 4) {
+            fprintf(stderr, "Incorrect argument count for rr.\n");
+            fprintf(stderr, "Usage: ./schedsim rr <input_file> <time_quantum>\n");
+            return 1;
+        }
+
+        quantum = atoi(argv[3]);
+        if (quantum <= 0) {
+            fprintf(stderr, "Invalid time quantum. Must be a positive integer.\n");
+            return 1;
+        }
+    }
+
+    // temp test output 
+    printf("Algorithm: %s\n", argv[1]);
+    printf("Input file: %s\n", argv[2]);
+
+    if (strcmp(argv[1], "rr") == 0) {
+        printf("Time quantum: %d\n", quantum);
+    }
+
+    return 0;
 }
