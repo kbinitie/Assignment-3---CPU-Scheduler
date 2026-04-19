4947049e6a9000d35bc50a0ba552d8acf26788ef
Brent Matthew Ortizo <144473184+BrentMatthewOrtizo@users.noreply.github.com>
2026-04-18 18:55:18 -0700

    fixed

--- schedsim.c
+++ schedsim.c
@@ -9,6 +9,7 @@ typedef struct process{
 } process;
 
 
+<<<<<<< HEAD
 int fileHandler(process *processlist, char *filename);
 
 int main (int argc, char *argv[]){
@@ -30,6 +31,77 @@ int main (int argc, char *argv[]){
       fprintf(stderr, "Incorrect argument count\n");
       fprintf(stderr, "Usage: ./<argv[0] <scheduling algorithm> <input file>");
       return -1;
+=======
+#define MAX_PROCESSES 100
+
+int load_processes(const char *filename, Process processes[]) {
+    FILE *input_file;
+    char header[100];
+    int count;
+
+    input_file = fopen(filename, "r");
+    if (input_file == NULL) {
+        fprintf(stderr, "Error: could not open input file %s\n", filename);
+        return -1;
+    }
+
+    // skip the header line
+    if (fgets(header, sizeof(header), input_file) == NULL) {
+        fprintf(stderr, "Error: input file is empty.\n");
+        fclose(input_file);
+        return -1;
+    }
+
+    count = 0;
+
+    while (count < MAX_PROCESSES &&
+           fscanf(input_file, "%19s %d %d %d",
+                  processes[count].pid,
+                  &processes[count].arrival,
+                  &processes[count].burst,
+                  &processes[count].priority) == 4) {
+
+        processes[count].remaining_burst = processes[count].burst;
+        processes[count].first_start_time = -1;
+        processes[count].completion_time = -1;
+        processes[count].started = 0;
+        processes[count].finished = 0;
+        processes[count].vruntime = 0;
+        processes[count].rr_time_used = 0;
+        processes[count].index = count;
+
+        count++;
+    }
+
+    fclose(input_file);
+    return count;
+}
+
+int main(int argc, char *argv[]) {
+    int quantum;
+
+    Process processes[MAX_PROCESSES];
+    int process_count;
+    int i;
+
+    // need at least program name + algorithm + input file
+    if (argc < 3 || argc > 4) { // prog requires either 3 or 4 total args
+        fprintf(stderr, "Incorrect argument count.\n");
+        fprintf(stderr, "Usage:\n");
+        fprintf(stderr, "  ./schedsim priority <input_file>\n");
+        fprintf(stderr, "  ./schedsim cfs <input_file>\n");
+        fprintf(stderr, "  ./schedsim rr <input_file> <time_quantum>\n");
+        return 1;
+    }
+
+    // we validate algorithm
+    if (strcmp(argv[1], "priority") != 0 &&
+        strcmp(argv[1], "rr") != 0 &&
+        strcmp(argv[1], "cfs") != 0) {
+        fprintf(stderr, "Invalid scheduling algorithm.\n");
+        fprintf(stderr, "Valid options are: priority, rr, cfs\n");
+        return 1;
+>>>>>>> 057963b (file handling)
     }
     if(strcmp(argv[1], "rr") == 0 && argc != 4){
       fprintf(stderr, "Incorrect argument count\n");
@@ -47,14 +119,25 @@ int main (int argc, char *argv[]){
   process *processlist = NULL; 
   fileHandler(processlist, argv[2]);
 
+<<<<<<< HEAD
   // Printing the processes to verify that they were read correctly
   for(int i = 0; i < sizeof(processlist); i++){
     printf("PID: %s, Arrival Time: %d, Burst Time: %d, Priority: %d\n", processlist[i].pid, processlist[i].arrivalTime, processlist[i].burstTime, processlist[i].priority);
   }
+=======
+    // rr should have exactly 4 args
+    if (strcmp(argv[1], "rr") == 0) {
+        if (argc != 4) {
+            fprintf(stderr, "Incorrect argument count for rr.\n");
+            fprintf(stderr, "Usage: ./schedsim rr <input_file> <time_quantum>\n");
+            return 1;
+        }
+>>>>>>> 057963b (file handling)
 
   return 0;
 }
 
+<<<<<<< HEAD
 // Function to read the input file and store the processes in a dynamic array
 int fileHandler(process *processlist, char *filename){
   FILE *file = fopen(filename, "r");
@@ -67,6 +150,38 @@ int fileHandler(process *processlist, char *filename){
    // Ignoring the first line of the file
     char buffer[100];
     fgets(buffer, sizeof(buffer), file);
+=======
+    // temp test output
+    printf("Algorithm: %s\n", argv[1]);
+    printf("Input file: %s\n", argv[2]);
+
+    if (strcmp(argv[1], "rr") == 0) {
+        printf("Time quantum: %d\n", quantum);
+    }
+
+    process_count = load_processes(argv[2], processes);
+    if (process_count == -1) {
+        return 1;
+    }
+
+    if (process_count == 0) {
+        fprintf(stderr, "Error: no processes found in input file.\n");
+        return 1;
+    }
+
+    printf("\nLoaded Processes:\n");
+    for (i = 0; i < process_count; i++) {
+        printf("Process %d:\n", i);
+        printf("  PID: %s\n", processes[i].pid);
+        printf("  Arrival: %d\n", processes[i].arrival);
+        printf("  Burst: %d\n", processes[i].burst);
+        printf("  Priority: %d\n", processes[i].priority);
+        printf("  Remaining Burst: %d\n", processes[i].remaining_burst);
+        printf("  Started: %d\n", processes[i].started);
+        printf("  Finished: %d\n", processes[i].finished);
+        printf("  Index: %d\n", processes[i].index);
+    }
+>>>>>>> 057963b (file handling)
 
    // Reading the file and storing the processes in a dynamic array
    while (fread(&temp, sizeof(process), 1, file) == 1)
