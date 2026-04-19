08d70fb7b087e883155180bdd1738d28313fe9fc
Brent Matthew Ortizo <144473184+BrentMatthewOrtizo@users.noreply.github.com>
2026-04-18 19:31:18 -0700

    semaphores

--- schedsim.c
+++ schedsim.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> // strcmp() changed since argv[1] is a string, compare strings with strcmp()
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #include "process.h"
 
 #define MAX_PROCESSES 100
+
+Process processes[MAX_PROCESSES];
+int process_count = 0;
+
+pthread_t process_threads[MAX_PROCESSES];
+pthread_t scheduler_thread;
+
+sem_t process_sems[MAX_PROCESSES];
+sem_t tick_done_sem;
+
+int current_time = 0;
+int finished_count = 0;
+int simulation_done = 0;
 
 int load_processes(const char *filename, Process processes[]) {
     FILE *input_file;
@@ -49,15 +64,81 @@ int load_processes(const char *filename, Process processes[]) {
     return count;
 }
 
-int main(int argc, char *argv[]) {
-    int quantum;
+void *process_runner(void *arg) {
+    Process *p = (Process *)arg;
 
-    Process processes[MAX_PROCESSES];
-    int process_count;
+    while (1) {
+        sem_wait(&process_sems[p->index]);
+
+        if (simulation_done) {
+            break;
+        }
+
+        if (!p->started) {
+            p->started = 1;
+            p->first_start_time = current_time;
+        }
+
+        if (p->remaining_burst > 0) {
+            p->remaining_burst--;
+            printf("Process %s ran for one tick. Remaining burst: %d\n",
+                   p->pid, p->remaining_burst);
+
+            if (p->remaining_burst == 0) {
+                p->finished = 1;
+                p->completion_time = current_time + 1;
+                finished_count++;
+                printf("Process %s finished at time %d\n",
+                       p->pid, p->completion_time);
+            }
+        }
+
+        sem_post(&tick_done_sem);
+    }
+
+    return NULL;
+}
+
+void *scheduler_runner(void *arg) {
     int i;
 
-    // need at least program name + algorithm + input file
-    if (argc < 3 || argc > 4) { // prog requires either 3 or 4 total args
+    printf("\nScheduler started.\n");
+
+    while (finished_count < process_count) {
+        for (i = 0; i < process_count; i++) {
+            if (processes[i].arrival <= current_time && !processes[i].finished) {
+                printf("\nScheduler: time %d, running %s\n",
+                       current_time, processes[i].pid);
+
+                sem_post(&process_sems[processes[i].index]);
+                sem_wait(&tick_done_sem);
+
+                current_time++;
+                break;
+            }
+        }
+
+        if (i == process_count) {
+            printf("\nScheduler: time %d, IDLE\n", current_time);
+            current_time++;
+        }
+    }
+
+    simulation_done = 1;
+
+    for (i = 0; i < process_count; i++) {
+        sem_post(&process_sems[i]);
+    }
+
+    printf("\nScheduler finished.\n");
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    int quantum;
+    int i;
+
+    if (argc < 3 || argc > 4) {
         fprintf(stderr, "Incorrect argument count.\n");
         fprintf(stderr, "Usage:\n");
         fprintf(stderr, "  ./schedsim priority <input_file>\n");
@@ -66,7 +147,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // we validate algorithm
     if (strcmp(argv[1], "priority") != 0 &&
         strcmp(argv[1], "rr") != 0 &&
         strcmp(argv[1], "cfs") != 0) {
@@ -75,14 +155,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // priority and cfs should have exactly 3 args
     if ((strcmp(argv[1], "priority") == 0 || strcmp(argv[1], "cfs") == 0) && argc != 3) {
         fprintf(stderr, "Incorrect argument count for %s.\n", argv[1]);
         fprintf(stderr, "Usage: ./schedsim %s <input_file>\n", argv[1]);
         return 1;
     }
 
-    // rr should have exactly 4 args
     if (strcmp(argv[1], "rr") == 0) {
         if (argc != 4) {
             fprintf(stderr, "Incorrect argument count for rr.\n");
@@ -97,7 +175,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // temp test output
     printf("Algorithm: %s\n", argv[1]);
     printf("Input file: %s\n", argv[2]);
 
@@ -127,6 +204,41 @@ int main(int argc, char *argv[]) {
         printf("  Finished: %d\n", processes[i].finished);
         printf("  Index: %d\n", processes[i].index);
     }
+
+    for (i = 0; i < process_count; i++) {
+        if (sem_init(&process_sems[i], 0, 0) != 0) {
+            fprintf(stderr, "Error initializing process semaphore %d\n", i);
+            return 1;
+        }
+    }
+
+    if (sem_init(&tick_done_sem, 0, 0) != 0) {
+        fprintf(stderr, "Error initializing tick_done semaphore\n");
+        return 1;
+    }
+
+    for (i = 0; i < process_count; i++) {
+        if (pthread_create(&process_threads[i], NULL, process_runner, &processes[i]) != 0) {
+            fprintf(stderr, "Error creating process thread %d\n", i);
+            return 1;
+        }
+    }
+
+    if (pthread_create(&scheduler_thread, NULL, scheduler_runner, NULL) != 0) {
+        fprintf(stderr, "Error creating scheduler thread\n");
+        return 1;
+    }
+
+    pthread_join(scheduler_thread, NULL);
+
+    for (i = 0; i < process_count; i++) {
+        pthread_join(process_threads[i], NULL);
+    }
+
+    for (i = 0; i < process_count; i++) {
+        sem_destroy(&process_sems[i]);
+    }
+    sem_destroy(&tick_done_sem);
 
     return 0;
 }
